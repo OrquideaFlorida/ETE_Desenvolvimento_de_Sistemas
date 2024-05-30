95efe736b4125bd010f8c1357db8d4d2d3babb3b
OrquideaFlorida <140519321+OrquideaFlorida@users.noreply.github.com>
2024-05-30 21:14:23 +0000

    Atividade Objeto

---
+++ Logica_Computacional/objeto.js
@@ -0,0 +1,16 @@
+let smartphone = {
+    marca: "Apple",
+    modelo: "iPhone 15",
+    ano: 2024,
+    série: "123-456-789-00",
+    tamanho: "6.7'",
+    cor: "Azul",
+    chips: 2,
+    telefones: ["(81)99999-9999, (81)98888-8888"],
+    ligando: function(){
+        return "Trim, trim, trim, telefonema!!!"
+    }
+}
+
+console.table(smartphone);
+console.log(smartphone.ligando());
