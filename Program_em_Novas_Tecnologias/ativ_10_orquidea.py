2011f504d7dad87f8fd336cffc1703984d98630b
OrquideaFlorida <140519321+OrquideaFlorida@users.noreply.github.com>
2024-05-31 01:33:05 +0000

    python

--- Program_em_Novas_Tecnologias/ativ_10_orquidea.py
+++ Program_em_Novas_Tecnologias/ativ_10_orquidea.py
@@ -1,3 +1,5 @@
+# Modifique o programa ilustrado na Atividade Extraclasse da Aula 10 usando for.
+# Explique por que nem todos os while podem ser transformados em for.
 L = []
 
 while True:
@@ -10,3 +12,20 @@ x = 0
 while x < len(L):
     print(L[x])
     x += 1
+
+#########################################################
+#RESOLUÇÃO
+L = []
+
+while True:
+    n = int(input("Digite um número (0 para sair): "))
+    if n == 0:
+        break
+    L.append(n)
+
+x = 0
+for x in L:
+    print(x)
+
+#O comando WHILE é usado quando se deseja realizar uma repetição que não tenha um limite estabelecido.
+#Já o FOR, serve para realizar uma repetição on já sabe o limite dela.
